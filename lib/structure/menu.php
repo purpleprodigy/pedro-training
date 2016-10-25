4e75536d7f8d0ecd337ab0352dab1f39ef4b68c7
Rose Cox <rose@purpleprodigy.com>
2016-10-25 14:42:19 +1000

    Create English page template to show English menu

--- lib/structure/menu.php
+++ lib/structure/menu.php
@@ -20,7 +20,7 @@ namespace PEDRO\PedroTraining;
  * @return mixed
  */
 function show_english_nav_menu( $args ){
-	if( ( 'primary' == $args['theme_location'] ) && (is_page( 18) || is_subpage() == 18 ) )
+	if( ( 'primary' == $args['theme_location'] ) && (is_page_template( 'english.php') ) )
 	{
 		$args['menu'] = 'English'; // Add your menu name here. My case it is "Menu for Page"
 	}
@@ -29,21 +29,3 @@ function show_english_nav_menu( $args ){
 }
 
 add_filter( 'wp_nav_menu_args', __NAMESPACE__ . '\show_english_nav_menu' );
-
-/**
- * Return ID of a parent page if a page has a parent.
- *
- * @since 1.0.0
- *
- * @return int
- */
-function is_subpage() {
-	global $post;
-
-	if ( is_page() && $post->post_parent ) {
-		return $post->post_parent;
-
-	} else {
-		return 0;
-	}
-}
