fd82741da6415080ee1a8450394779033e5e9c11
Rose Cox <rose@purpleprodigy.com>
2016-11-24 15:13:54 +1000

    Remove Contact link from footer

--- lib/structure/footer.php
+++ lib/structure/footer.php
@@ -21,13 +21,14 @@ add_filter( 'genesis_footer_creds_text', __NAMESPACE__ . '\pp_footer_creds_filte
  * @return string
  */
 function pp_footer_creds_filter( $creds ) {
-
-	$creds = 'Copyright [footer_copyright] <a href="https://training.pedro.org.au">2016 Physiotherapy Evidence Database (PEDro)</a> &middot; <a href="/contact/">Contact</a> &middot; <a href="/sitemap/" rel="nofollow">Sitemap</a> &middot; <a href="/terms/" rel="nofollow">Terms &amp; Conditions</a> &middot; Website by <a href="http://www.purpleprodigy.com">Purple Prodigy</a>
-<div class="social-icons"><ul><li><a href="http://www.facebook.com/PhysiotherapyEvidenceDatabase.PEDro"><i class="fa fa-facebook"><span class="screen-reader-text">Like us on Facebook</span></i></a></li>
-			<li><a href="https://twitter.com/#!/PEDro_CEBP"><i class="fa fa-twitter"><span class="screen-reader-text">Follow us on Twitter</span></i></a></li>
-			<li><a href="https://plus.google.com/101086467607648511656"><i class="fa fa-google-plus"><span class="screen-reader-text">Connect on Google+</span></i></a></li>
-			<li><a href="https://www.youtube.com/channel/UCOz1EVhtpOMMQRzRioGIOtQ"><i class="fa fa-youtube"><span class="screen-reader-text">Follow us on YouTube</span></i></a></li></ul></div>';
-
+	$creds = '<p>Copyright [footer_copyright] <a href="https://training.pedro.org.au">Physiotherapy Evidence Database (PEDro)</a> &middot; <a href="/sitemap/" rel="nofollow">Sitemap</a> &middot; <a href="/terms/" rel="nofollow">Terms &amp; Conditions</a> &middot; Website by <a href="http://www.purpleprodigy.com">Purple Prodigy</a></p>
+			<div class="social-icons">
+				<ul>
+				<li><a href="http://www.facebook.com/PhysiotherapyEvidenceDatabase.PEDro"><i class="fa fa-facebook"><span class="screen-reader-text">Like us on Facebook</span></i></a></li>
+				<li><a href="https://twitter.com/#!/PEDro_CEBP"><i class="fa fa-twitter"><span class="screen-reader-text">Follow us on Twitter</span></i></a></li>
+				<li><a href="https://plus.google.com/101086467607648511656"><i class="fa fa-google-plus"><span class="screen-reader-text">Connect on Google+</span></i></a></li>
+				<li><a href="https://www.youtube.com/channel/UCOz1EVhtpOMMQRzRioGIOtQ"><i class="fa fa-youtube"><span class="screen-reader-text">Follow us on YouTube</span></i></a></li>
+				</ul>
+			</div>';
 	return $creds;
-
 }
